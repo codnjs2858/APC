2dacce6849aba39d1a3fb7fd3675197ffd4c5170
kyungjin0904 <kyungjin0904@naver.com>
2020-06-09 10:14:05 +0900

    [유기동물보호센터] 레이아웃 변경 작성자:이경진 detail: 레이아웃 변경에 따른 각종 css, plugin 파일 추가

---
+++ AnimalProtectionCenter/src/main/java/com/cafe24/memory/domain/AnimalType.java
@@ -0,0 +1,40 @@
+package com.cafe24.memory.domain;
+
+import java.util.Date;
+
+public class AnimalType {
+	private String animalTypeCode;
+	private String animalType;
+	private String animalTypeSub;
+	private Date animalTypeRegDate;
+	public String getAnimalTypeCode() {
+		return animalTypeCode;
+	}
+	public void setAnimalTypeCode(String animalTypeCode) {
+		this.animalTypeCode = animalTypeCode;
+	}
+	public String getAnimalType() {
+		return animalType;
+	}
+	public void setAnimalType(String animalType) {
+		this.animalType = animalType;
+	}
+	public String getAnimalTypeSub() {
+		return animalTypeSub;
+	}
+	public void setAnimalTypeSub(String animalTypeSub) {
+		this.animalTypeSub = animalTypeSub;
+	}
+	public Date getAnimalTypeRegDate() {
+		return animalTypeRegDate;
+	}
+	public void setAnimalTypeRegDate(Date animalTypeRegDate) {
+		this.animalTypeRegDate = animalTypeRegDate;
+	}
+	@Override
+	public String toString() {
+		return "AnimalType [animalTypeCode=" + animalTypeCode + ", animalType=" + animalType + ", animalTypeSub="
+				+ animalTypeSub + ", animalTypeRegDate=" + animalTypeRegDate + "]";
+	}
+	
+}
