3d57e28424fc8f6e003f38a9f294acf780f29f17
cndrl2148 <cndrl2148@gmail.com>
2020-06-10 11:36:50 +0900

    [유기동물보호센터] 유기동물보호관리, 유기동물보호장소
    file:animal
    detail:인덱스 화면 폼 내용 추가2

--- AnimalProtectionCenter/src/main/java/com/cafe24/memory/controller/AnimalProtectionController.java
+++ AnimalProtectionCenter/src/main/java/com/cafe24/memory/controller/AnimalProtectionController.java
@@ -1,14 +1,31 @@
 package com.cafe24.memory.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AnimalProtectionController {
 	
 	@GetMapping("/animalProtection")
-	public String animalProtection() {
+	public String animalProtection(Model model) {
 		System.out.println("!!!");
+		
+		return "animalprotect/animalProctectSpace";
+	}
+	
+	@GetMapping("/addAnimalProtection")
+	public String addAnimalProtection(Model model, 
+			@RequestParam(value = "space", required = false) String space) {
+		model.addAttribute("space", space);
+		
+		return "animalprotect/animalProtectInsert";
+	}
+	
+	@PostMapping("/animalProtection")
+	public String animalProtection() {
 		
 		return "animalprotect/animalProctectSpace";
 	}
