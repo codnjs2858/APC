a735b46ec3dbaba0496f30d659255f545e018689
codnjs2858 <codnjs2858@naver.com>
2020-06-15 17:01:05 +0900

    [유기동물보호센터] AnimalType 작성자:설채원 detail : AnimalTypeInsert AnimalTypeList List<Map<String,Object>> 를 써서 해결했다.

--- AnimalProtectionCenter/src/main/java/com/cafe24/memory/controller/AnimalTypeController.java
+++ AnimalProtectionCenter/src/main/java/com/cafe24/memory/controller/AnimalTypeController.java
@@ -1,26 +1,60 @@
 package com.cafe24.memory.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.cafe24.memory.domain.AnimalType;
+import com.cafe24.memory.service.AnimalTypeService;
 
 @Controller
 public class AnimalTypeController {
-	//animal type
+	@Autowired private AnimalTypeService animalTypeService;
+	
+	//animal type list
 	@GetMapping("/animaltypelist")
-	public String listAnimalType() {
+	public String listAnimalType(Model model) {
+		model.addAttribute("Cnt", animalTypeService.selectAnimalCnt());
+		model.addAttribute("TypeList", animalTypeService.selectAnimalType());
 		return "animaltype/animalTypeList";
 	}
+	@GetMapping("/animaltypeselect")
+	public String selectAnimalType(String animalType,Model model) {
+		model.addAttribute("Cnt", animalTypeService.selectAnimalCnt());
+		System.out.println(animalType);
+		return "animaltype/animalTypeList";
+	}
+	//animal type insert
 	@GetMapping("/animaltypeinsert")
-	public String insertAnimalType() {
+	public String insertAnimalForm() {
 		return "animaltype/animalTypeInsert";
 	}
+	@PostMapping("/animaltypeinsert")
+	public String insertAnimalType(AnimalType atype) {
+		animalTypeService.insertAnimalType(atype);
+		return "redirect:/animaltypelist"; 
+	}
+	//animal type update
 	@GetMapping("/animaltypeupdate")
-	public String updateAnimalType() {
+	public String updateAnimalForm(
+			@RequestParam(name="send_code", required = false) String send_code, Model model) {
+		model.addAttribute("type",animalTypeService.selectType(send_code));
 		return "animaltype/animalTypeUpdate";
 	}
+	@PostMapping("/animaltypeupdate")
+	public String updateAnimalType(AnimalType atype) {
+		animalTypeService.updateAnimalType(atype);
+		return "redirect:/animaltypelist";
+	}
+	
+	//animal type delete
 	@GetMapping("/animaltypedelete")
-	public String deleteAnimalType() {
-		//경로 delete
-		return "animaltype/animalTypeList";
+	public String deleteAnimalType(@RequestParam(name="send_code", required = false) String send_code) {
+		System.out.println(send_code);
+		animalTypeService.deleteAnimalType(send_code);
+		return "redirect:/animaltypelist";
 	}
 }
