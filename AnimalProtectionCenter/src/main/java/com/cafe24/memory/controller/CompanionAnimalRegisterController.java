a3c13b91e84d15d619733770c1c721a9b403999a
smy7414 <aceguy0521@gmail.com>
2020-06-15 17:00:53 +0900

    [AnimalProtectionCenter] 작성자 : 손민용 내용 : CompanionAnimalRegister 리스트작업, 인서트작업중 file :  CompanionAnimalRegisterController, 		CompanionAnimalRegisterMapper, 		CompanionAnimalRegisterService, 		companionAnimalRegisterList, 		companionAnimalRegisterInsert

--- AnimalProtectionCenter/src/main/java/com/cafe24/memory/controller/CompanionAnimalRegisterController.java
+++ AnimalProtectionCenter/src/main/java/com/cafe24/memory/controller/CompanionAnimalRegisterController.java
@@ -1,40 +1,60 @@
 package com.cafe24.memory.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.cafe24.memory.domain.CompanionAnimalRegister;
+import com.cafe24.memory.service.CompanionAnimalRegisterService;
+
 @Controller
 public class CompanionAnimalRegisterController {
 	
+	@Autowired
+	CompanionAnimalRegisterService companionAnimalRegisterService;
+	
 	@GetMapping("/companionAnimalRegisterList")
 	
-		public String companionAnimalRegisterList() {
+		public String companionAnimalRegisterList(Model model) {
+			
+			model.addAttribute("companionAnimalRegisterList", companionAnimalRegisterService.selectCompanionAnimalRegisterList());
 			return "companionanimalregister/companionAnimalRegisterList";
 		
-	}
+		}
 	
 	@PostMapping("/companionAnimalRegisterInsert")
 	
-		public String companionAnimalRegisterInsert() {
-			return "companionanimalregister/companionAnimalRegisterList";
+		public String companionAnimalRegisterInsert(Model model 
+													, CompanionAnimalRegister companionAnimalRegister) {
+			
+			model.addAttribute("mlist" , companionAnimalRegisterList(model));
+			
+			return "redirect:/companionAnimalRegisterList";
 		
-	}
+		}
 	
 	@GetMapping("/companionAnimalRegisterInsert")
 	
 		public String companionAnimalRegisterInsert(Model model) {
+			List<String> memberIdList = companionAnimalRegisterService.selectMemberAll();
+			System.out.println(memberIdList);
+			
+			model.addAttribute("mlist", memberIdList);
+			
 			return "companionanimalregister/companionAnimalRegisterInsert";
 		
-	}
+		}
 	
 	@GetMapping("/companionAnimalRegisterUpdate")
 	
 		public String companionAnimalRegisterUpdate() {
 			return "companionanimalregister/companionAnimalRegisterUpdate";
 		
-	}
+		}
 	
 	@PostMapping("/companionAnimalRegisterUpdate")
 	
@@ -42,6 +62,6 @@ public class CompanionAnimalRegisterController {
 			return "companionanimalregister/companionAnimalRegisterList";
 			
 		
-	}
+		}
 	
 }
