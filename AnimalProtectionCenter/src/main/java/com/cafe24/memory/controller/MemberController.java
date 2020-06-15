80f7773597ba3854578c95f8a2534b79437b8276
kyungjin0904 <kyungjin0904@naver.com>
2020-06-15 17:03:14 +0900

    [유기동물보호센터] 작성자:이경진 detail:회원리스트,회원수정,로그인/회원가입폼 수정

--- AnimalProtectionCenter/src/main/java/com/cafe24/memory/controller/MemberController.java
+++ AnimalProtectionCenter/src/main/java/com/cafe24/memory/controller/MemberController.java
@@ -1,15 +1,19 @@
 package com.cafe24.memory.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.cafe24.memory.domain.Member;
+import com.cafe24.memory.service.MemberService;
 
 @Controller
 public class MemberController {
-
+	@Autowired private MemberService memberService;
+	
 	@GetMapping("/login")
 	public String login() {
 	
@@ -35,4 +39,32 @@ public class MemberController {
 		model.addAttribute("title", "회원가입");
 		return "index";
 	}
+	@GetMapping("/adminAddMember")
+	public String addMebmer(Model model) {
+
+		return "member/adminAddMember";
+	}
+	@GetMapping("/getMemberList")
+	public String getMemberList(Model model) {
+		model.addAttribute("memberList", memberService.getMemberList());
+		return "member/memberList";
+	}
+	@GetMapping("/memberUpdate")
+	public String memberUpdate(Model model,Member member) {
+		
+		model.addAttribute("mem", member.getMemberId());
+		return "member/updateMember";
+	}
+	@PostMapping("/memberUpdate")
+	public String memberUpdate(Member member) {
+
+		return "index";
+	}
+	
+	@GetMapping("/memberDelete")
+	public String memberDelete(Member member) {
+		memberService.deleteMember(member.getMemberId());
+		return "index";
+	}
+	
 }
