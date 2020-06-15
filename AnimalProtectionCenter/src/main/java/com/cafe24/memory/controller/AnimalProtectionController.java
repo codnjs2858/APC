b65f13345964cc1f2525e5a9924d3628868ad3f5
cndrl2148 <cndrl2148@gmail.com>
2020-06-15 16:44:15 +0900

    [유기동물보호센터] 디펜더스, 에이작스 작성자:손충기 파일:pom.xml detail:초기설정이 잘못 되어있어서 다시 세팅하고 에이작스를 통해 동물이름 중복검사를 만들어봄

--- AnimalProtectionCenter/src/main/java/com/cafe24/memory/controller/AnimalProtectionController.java
+++ AnimalProtectionCenter/src/main/java/com/cafe24/memory/controller/AnimalProtectionController.java
@@ -1,13 +1,20 @@
 package com.cafe24.memory.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cafe24.memory.domain.AnimalProtect;
 import com.cafe24.memory.domain.ProtectionSpace;
@@ -18,6 +25,21 @@ public class AnimalProtectionController {
 	
 	@Autowired
 	ProtectionService protectionService;
+	
+	//session 에 있는 id
+	String loginId = "id001"; 
+
+	
+	/* @RequestMapping(value = "addProtectInsert", method = RequestMethod.POST ) */
+	@PostMapping("/addProtectInsert")
+	@ResponseBody
+	public String addProtectInsert(@RequestParam(name = "btnNamePet") String petName) {
+		System.out.println(petName);
+		
+		petName = petName + "1213123";
+		
+		return petName;
+	}
 	
 	@GetMapping("/animalProtection")
 	public String animalProtection(Model model) {
