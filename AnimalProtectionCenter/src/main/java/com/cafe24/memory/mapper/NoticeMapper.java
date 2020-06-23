fd3a464fff30fca7b7d4c00a7e6d1c1451ba6323
smart-202-5 <smart-202-5@smart-202-5-PC>
2020-06-23 15:21:06 +0900

    게시글 열람 기능 추가

--- AnimalProtectionCenter/src/main/java/com/cafe24/memory/mapper/NoticeMapper.java
+++ AnimalProtectionCenter/src/main/java/com/cafe24/memory/mapper/NoticeMapper.java
@@ -9,5 +9,6 @@ import com.cafe24.memory.domain.Notice;
 @Mapper
 public interface NoticeMapper {
 	public int insertNotice(Notice notice);
+	public List<Notice> selectPosts(Notice notice);
 	public List<Notice> selectNotice();
 }
