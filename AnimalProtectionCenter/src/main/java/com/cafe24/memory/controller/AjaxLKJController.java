1decddfdf38ee16dd7573c3fd0f0b8c43d3ee115
kyungjin0904 <kyungjin0904@naver.com>
2020-06-25 16:34:35 +0900

    [유기동물센터] 작성자:이경진 detail:회원가입폼 아이디 중복성검사

--- AnimalProtectionCenter/src/main/java/com/cafe24/memory/controller/AjaxLKJController.java
+++ AnimalProtectionCenter/src/main/java/com/cafe24/memory/controller/AjaxLKJController.java
@@ -1,10 +1,38 @@
 package com.cafe24.memory.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.cafe24.memory.domain.Member;
+import com.cafe24.memory.service.MemberService;
+
+@RestController
 @RequestMapping("/ajax")
 public class AjaxLKJController {
+	@Autowired private MemberService memberService;
+	
+	/**
+	 * 회원가입시 기존 아이디 존재하는지 중복확인-이경진
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/Addmember")
+	public String ajaxAddmember(@RequestParam(value = "id") String id) {
+		System.out.println(id);
+		Member member= new Member();
+		member.setMemberId(id);
+		List<Member>mList=memberService.getMemberList(member);
+		String result="";
+		if(mList != null && !"".equals(mList.get(0).getMemberId())){
+			result="N";
+		}
+		return result;
+	}
 
 }
