0d7de42a145fa1ab4881d600de44df24a4437d1f
kyungjin0904 <kyungjin0904@naver.com>
2020-07-01 16:53:14 +0900

    [유기동물센터] 작성자:이경진 분실신고 등록 쿼리작성

--- AnimalProtectionCenter/src/main/java/com/cafe24/memory/domain/ReportManger.java
+++ AnimalProtectionCenter/src/main/java/com/cafe24/memory/domain/ReportManger.java
@@ -2,16 +2,25 @@ package com.cafe24.memory.domain;
 
 public class ReportManger {
 	private String acceptCode;
+	private SearchReportAnimal searchReport;
 	private String reportCode;
+	private LostReportAnimal lostReport;
 	private String acceptState;
-	private Member memberId;
+	private Member member;
 	private String acceptInDate;
 	private String acceptOutDate;
+	
 	public String getAcceptCode() {
 		return acceptCode;
 	}
 	public void setAcceptCode(String acceptCode) {
 		this.acceptCode = acceptCode;
+	}
+	public SearchReportAnimal getSearchReport() {
+		return searchReport;
+	}
+	public void setSearchReport(SearchReportAnimal searchReport) {
+		this.searchReport = searchReport;
 	}
 	public String getReportCode() {
 		return reportCode;
@@ -19,17 +28,23 @@ public class ReportManger {
 	public void setReportCode(String reportCode) {
 		this.reportCode = reportCode;
 	}
+	public LostReportAnimal getLostReport() {
+		return lostReport;
+	}
+	public void setLostReport(LostReportAnimal lostReport) {
+		this.lostReport = lostReport;
+	}
 	public String getAcceptState() {
 		return acceptState;
 	}
 	public void setAcceptState(String acceptState) {
 		this.acceptState = acceptState;
 	}
-	public Member getMemberId() {
-		return memberId;
+	public Member getMember() {
+		return member;
 	}
-	public void setMemberId(Member memberId) {
-		this.memberId = memberId;
+	public void setMember(Member member) {
+		this.member = member;
 	}
 	public String getAcceptInDate() {
 		return acceptInDate;
@@ -45,9 +60,9 @@ public class ReportManger {
 	}
 	@Override
 	public String toString() {
-		return "ReportManger [acceptCode=" + acceptCode + ", reportCode=" + reportCode + ", acceptState=" + acceptState
-				+ ", memberId=" + memberId + ", acceptInDate=" + acceptInDate + ", acceptOutDate=" + acceptOutDate
-				+ "]";
+		return "ReportManger [acceptCode=" + acceptCode + ", searchReport=" + searchReport + ", reportCode="
+				+ reportCode + ", lostReport=" + lostReport + ", acceptState=" + acceptState + ", member=" + member
+				+ ", acceptInDate=" + acceptInDate + ", acceptOutDate=" + acceptOutDate + "]";
 	}
 	
 	
