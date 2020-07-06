4c4d332938dde470ebf3eb460a1ada1e28b9900a
smart-202-5 <smart-202-5@smart-202-5-PC>
2020-07-06 14:49:39 +0900

    입양심사 데이터 넘기기 & note, board 세션값 불러오기 완료

---
+++ AnimalProtectionCenter/src/main/java/com/cafe24/memory/domain/AdoptionReview.java
@@ -0,0 +1,110 @@
+package com.cafe24.memory.domain;
+
+public class AdoptionReview {
+	private String first;
+	private String second;
+	private String therd;
+	private String fourth;
+	private String fifth;
+	private String sixth;
+	private String seventh;
+	private String eighth;
+	private String ninth;
+	private String tenth;
+	private String eleventh;
+	
+	public String getFirst() {
+		return first;
+	}
+
+	public void setFirst(String first) {
+		this.first = first;
+	}
+
+	public String getSecond() {
+		return second;
+	}
+
+	public void setSecond(String second) {
+		this.second = second;
+	}
+
+	public String getTherd() {
+		return therd;
+	}
+
+	public void setTherd(String therd) {
+		this.therd = therd;
+	}
+
+	public String getFourth() {
+		return fourth;
+	}
+
+	public void setFourth(String fourth) {
+		this.fourth = fourth;
+	}
+
+	public String getFifth() {
+		return fifth;
+	}
+
+	public void setFifth(String fifth) {
+		this.fifth = fifth;
+	}
+
+	public String getSixth() {
+		return sixth;
+	}
+
+	public void setSixth(String sixth) {
+		this.sixth = sixth;
+	}
+
+	public String getSeventh() {
+		return seventh;
+	}
+
+	public void setSeventh(String seventh) {
+		this.seventh = seventh;
+	}
+
+	public String getEighth() {
+		return eighth;
+	}
+
+	public void setEighth(String eighth) {
+		this.eighth = eighth;
+	}
+
+	public String getNinth() {
+		return ninth;
+	}
+
+	public void setNinth(String ninth) {
+		this.ninth = ninth;
+	}
+
+	public String getTenth() {
+		return tenth;
+	}
+
+	public void setTenth(String tenth) {
+		this.tenth = tenth;
+	}
+
+	public String getEleventh() {
+		return eleventh;
+	}
+
+	public void setEleventh(String eleventh) {
+		this.eleventh = eleventh;
+	}
+
+	@Override
+	public String toString() {
+		return "adoptionReview [first=" + first + ", second=" + second + ", therd=" + therd + ", fourth=" + fourth
+				+ ", fifth=" + fifth + ", sixth=" + sixth + ", seventh=" + seventh + ", eighth=" + eighth + ", ninth="
+				+ ninth + ", tenth=" + tenth + ", eleventh=" + eleventh + "]";
+	}
+}
