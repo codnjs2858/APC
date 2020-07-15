67eeff5832744bbc704718011e1aa02f9ea87b6b
cndrl2148 <cndrl2148@gmail.com>
2020-07-15 17:21:06 +0900

    o

--- AnimalProtectionCenter/src/main/java/com/cafe24/memory/interceptor/LoginInterceptor.java
+++ AnimalProtectionCenter/src/main/java/com/cafe24/memory/interceptor/LoginInterceptor.java
@@ -29,35 +29,41 @@ public class LoginInterceptor implements HandlerInterceptor{
 		String sessionId=(String)session.getAttribute("SID");
 		String sessionLevel=(String)session.getAttribute("SLEVEL");
 		String uri=request.getRequestURI();
+		
+		if(uri.equals("/admin")) {
+			if((sessionLevel.equals("level_code_03"))) {
+				response.sendRedirect("/index");
+				
+				return false; 
+			} 
+		}else if(uri.equals("/")) {
+			if(sessionId==null) {
+				response.sendRedirect("/index");
+			
+				return false;
+			}
+			else if((sessionLevel.equals("level_code_04"))) {
+				response.sendRedirect("/member/blacklistMemberAlert");
+				
+				return false; 
+			}
+		}else if(uri.equals("/adoption/adoptionApply")) {
+			Adoptee adoptee = adopteeService.selectAdopteeById(sessionId);
+			if(adoptee == null) {
+				response.sendRedirect("/adoption/adoptionReview");
+				
+				return false;
+			}else if(!"screening_complete_code_01".equals(adoptee.getScreeningComplete().getScreening_complete_code())){
+				response.sendRedirect("/adoption/adoptionReview");
+				
+				return false;
+			}
+		}
+		
 		if(sessionId==null) {
 			response.sendRedirect("/member/login");
 		
 			return false;
-		}else {
-			if(uri.equals("/admin")) {
-				if((sessionLevel.equals("level_code_03"))) {
-					response.sendRedirect("/index");
-					
-					return false; 
-				} 
-			}else if(uri.equals("/")) {
-				if((sessionLevel.equals("level_code_04"))) {
-					response.sendRedirect("/member/blacklistMemberAlert");
-					
-					return false; 
-				}
-			}else if(uri.equals("/adoption/adoptionApply")) {
-				Adoptee adoptee = adopteeService.selectAdopteeById(sessionId);
-				if(adoptee == null) {
-					response.sendRedirect("/adoption/adoptionReview");
-					
-					return false;
-				}else if(!"screening_complete_code_01".equals(adoptee.getScreeningComplete().getScreening_complete_code())){
-					response.sendRedirect("/adoption/adoptionReview");
-					
-					return false;
-				}
-			}
 		}
 		 
 	 	return HandlerInterceptor.super.preHandle(request, response, handler);	 
