8c6bc9954da5506a82d924a1ef1b5e2d24e158d3
cndrl2148 <cndrl2148@gmail.com>
2020-07-17 00:53:01 +0900

    [유기동물보호센터] index 화면 작성자:손충기 설명:일단 내맘대로 구상해서 만들어 보긴함

--- AnimalProtectionCenter/src/main/java/com/cafe24/memory/controller/AdminController.java
+++ AnimalProtectionCenter/src/main/java/com/cafe24/memory/controller/AdminController.java
@@ -1,5 +1,7 @@
 package com.cafe24.memory.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,12 +9,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cafe24.memory.domain.Board;
+import com.cafe24.memory.domain.LostReportAnimal;
+import com.cafe24.memory.domain.SearchReportAnimal;
+import com.cafe24.memory.service.AnimalCenterService;
+import com.cafe24.memory.service.AnimalReportService;
+import com.cafe24.memory.service.NoticeService;
 import com.cafe24.memory.service.StaffService;
 
 @Controller
 public class AdminController {
-	@Autowired private StaffService staffService;
+	
+	@Autowired 
+	private StaffService staffService;
+	
+	@Autowired
+	private AnimalCenterService animalCenterService;
+	
+	@Autowired
+	private AnimalReportService animalReportService;
+	
+	@Autowired
+	private NoticeService noticeService;
 	
 	@GetMapping("/")
 	public String intro(HttpSession session) {
@@ -22,13 +42,23 @@ public class AdminController {
 		}
 		return "intro/intro";
 	}
-	@GetMapping("/index")
-	public String index(Model model) {
-		return "index";
-	}
 	
-	@PostMapping("/index")
-	public String index() {
+	@RequestMapping("/index")
+	public String index(Model model) {
+		//여기에서 고양이 보내주고 하면 될듯 
+		List<SearchReportAnimal> searchReportAnimalList = animalReportService.selectSearchReportAnimal();
+		System.out.println(searchReportAnimalList + " <-- searchReportAnimalList");
+		List<LostReportAnimal> lostReportAniamlList = animalReportService.selectLostReportAnimal();
+		System.out.println(lostReportAniamlList + " <-- lostReportAniamlList");
+		List<Board> selectNoticeList = noticeService.selectNotice();
+		System.out.println(selectNoticeList + " <-- selectNoticeList");
+		
+		
+		model.addAttribute("cnt", animalCenterService.selectCenterCnt());
+		model.addAttribute("searchReportAnimalList", searchReportAnimalList);
+		model.addAttribute("lostReportAniamlList", lostReportAniamlList);
+		model.addAttribute("selectNoticeList", selectNoticeList);
+		
 		return "index";
 	}
 	
